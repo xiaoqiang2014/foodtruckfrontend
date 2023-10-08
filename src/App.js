9b00516c23d1acf0d57b54ac9651de2d248770ff
unknown <liqiang4007@gmail.com>
2023-10-08 11:08:32 +0800

    init change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [foodTrucks, setFoodTrucks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSearch = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(`localhost:3000/users/search?term=${searchTerm}`);
+      const data = await response.json();
+      setFoodTrucks(data);
+    } catch (error) {
+      console.error('Error fetching food trucks:', error);
+    }
+
+    setIsLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Food Truck Search</h1>
+
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button onClick={handleSearch} disabled={isLoading}>
+          {isLoading ? 'Loading...' : 'Search'}
+        </button>
+      </div>
+
+      {foodTrucks.length > 0 ? (
+        <ul>
+          {foodTrucks.map((foodTruck) => (
+            <li key={foodTruck.id}>{foodTruck.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No food trucks found.</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
